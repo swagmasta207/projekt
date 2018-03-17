1b4f0cd80e253ae9078bb20f6f393c097728a819
swagmasta207 <36759118+swagmasta207@users.noreply.github.com>
2018-03-17 20:50:08 +0200

    html baas kohaga erinevatele funktsioonidele, blue

---
+++ projekthtml/js_table_box.js
@@ -0,0 +1,13 @@
+function CalendarOn() {
+    document.getElementById("date").style.display = "block";
+}
+
+function CalendarOff() {
+
+    document.getElementById("date").style.display = "none";
+}
+
+function ClickAgain(){
+
+	document.getElementById("CalendarButton").click(0);
+}
